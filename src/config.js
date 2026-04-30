5efa956e87f2bee659e6a90b43ecf57f0fcf5656
juliancho143-source <juliancho143@gmail.com>
2026-04-30 14:02:09 -0500

    config & conexion fix

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 // ============================================================
 //  config.js — Variables globales de GoldenJulian
-//  Cambia aquí cualquier valor sin tocar el resto del código.
 // ============================================================
 
 export const CONFIG = {
@@ -11,22 +10,13 @@ export const CONFIG = {
   WHATSAPP_MENSAJE_BASE: 'Hola GoldenManillas, me interesa obtener más información de:',
 
   // ── Firebase ──────────────────────────────────────────────
-  //FIREBASE_CONFIG: {
-    //apiKey:            process.env.API_KEY_FIREBASE,
-    //authDomain:        process.env.AUTH_DOMAIN,
-    //projectId:         process.env.PROJECT_ID,
-    //storageBucket:     process.env.STORAGE_BUCKET,
-    //messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    //appId:             process.env.APP_ID,
- // },
-
   FIREBASE_CONFIG: {
-    apiKey: import.meta.env.VITE_API_KEY_FIREBASE,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID
+    apiKey:            import.meta.env.VITE_API_KEY_FIREBASE            || 'AIzaSyAxTyDOPzOPkWHgLu7iy4lcCxbRyxG_Akk',
+    authDomain:        import.meta.env.VITE_AUTH_DOMAIN                 || 'golden-julian.firebaseapp.com',
+    projectId:         import.meta.env.VITE_PROJECT_ID                  || 'golden-julian',
+    storageBucket:     import.meta.env.VITE_STORAGE_BUCKET              || 'golden-julian.firebasestorage.app',
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID         || '599121397465',
+    appId:             import.meta.env.VITE_APP_ID                      || '1:599121397465:web:d7f3022883cf58e560a0c8',
   },
 
   COLECCION_JOYAS: 'joyas',
